aba7294e7cbd0d4b575530f3a28974ca365bfd70
MitchelKwint <45388691+MitchelKwint@users.noreply.github.com>
2019-06-17 10:53:04 +0200

    Add files via upload

--- Frontend/Javascript H2/2.10/mijnfunctions.js
+++ Frontend/Javascript H2/2.10/mijnfunctions.js
@@ -1,6 +1,16 @@
-function afmelden(){
+function afmelden() {
     var antwoord = confirm('Wilt u zich afmelden');
-    if(antwoord == true ){
+    if (antwoord == true) {
         alert('U wordt afgemeld!!!');
+    }
+}
+
+function begroeten() {
+    var datum = new Date();
+    var uur = datum.getHours();
+    if (uur = > 0 && < 12) {
+        document.write('Goedemorgen');
+    } else if (uur = > 12 && < 18) {
+        document.write('Goedenavond');
     }
 }
